5bd19af04b1ef986119b31fdd21e60f176554763
Moses-Morris <mosesmorrisdev@gmail.com>
2024-03-14 17:36:46 +0300

    Register model to Admin for the admin to access it.

--- FeaturedApp/blog/views.py
+++ FeaturedApp/blog/views.py
@@ -2,6 +2,26 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from .models import Post
 
+
+# Create your views here.
+def index(request):
+    context = {
+        'posted' : Post.objects.all()
+    }
+    #return HttpResponse("<p>Hello, world. You're at the blog index.</p>")
+    return render(request, 'index.html', context)
+
+
+
+def about(request):
+    #return HttpResponse("<p>Hello, world. You're at the blog about.</p>")
+    return render(request, 'about.html', { 'pagename': 'About'})
+
+
+
+
+
+
 posts = [
     {
         'author': 'CoreyMS',
@@ -17,16 +37,3 @@ posts = [
     }
 
 ]
-# Create your views here.
-def index(request):
-    context = {
-        'posted' : Post.objects.all()
-    }
-    #return HttpResponse("<p>Hello, world. You're at the blog index.</p>")
-    return render(request, 'index.html', context)
-
-
-
-def about(request):
-    #return HttpResponse("<p>Hello, world. You're at the blog about.</p>")
-    return render(request, 'about.html', { 'pagename': 'About'})
