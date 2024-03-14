a2bf4370fd582d5f067bcafb8582efd55a60e56f
Moses-Morris <mosesmorrisdev@gmail.com>
2024-03-14 21:56:02 +0300

    Create a new app to handle users registration and informationa and add config to the project admin file..

--- FeaturedApp/users/views.py
+++ FeaturedApp/users/views.py
@@ -1,3 +1,21 @@
 from django.shortcuts import render
+from django.contrib.auth.forms import UserCreationForm
+
+
 
 # Create your views here.
+def register(request):
+    form = UserCreationForm()
+    context = {
+        'title': 'Register',
+        'content': 'Welcome to the registration page',
+        'form': form
+    }
+    return  render(request, 'register.html' , context)
+
+def login(request):
+    context = {
+        'title': 'Login',
+        'content': 'Welcome to the registration page'
+    }
+    return render(request, 'login.html', context)
