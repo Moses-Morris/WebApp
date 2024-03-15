401090450d0adc45dd8b0cb490ed5408790fe759
Moses-Morris <mosesmorrisdev@gmail.com>
2024-03-15 10:53:48 +0300

    Handle User Registration and redirect.

--- FeaturedApp/users/urls.py
+++ FeaturedApp/users/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.register, name='blogRegister'),
+    path('register', views.register, name='blogRegister'),
     path('login/', views.login, name='blogLogin'),
 ]
--- FeaturedApp/users/views.py
+++ FeaturedApp/users/views.py
@@ -1,11 +1,22 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.contrib.auth.forms import UserCreationForm
-
+from django.contrib import messages #messages.warning, messages. , messages.error, ,messages.success
 
 
 # Create your views here.
+
 def register(request):
-    form = UserCreationForm()
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            username = form.cleaned_data.get('username') #form.cleaned_data['username'] #or ')
+            messages.success(request, f'Account created for {username}!')
+            return redirect('blogLogin')
+        else:
+            messages.error(request, f'Account not created!')
+    else:
+        form = UserCreationForm()
+
     context = {
         'title': 'Register',
         'content': 'Welcome to the registration page',
