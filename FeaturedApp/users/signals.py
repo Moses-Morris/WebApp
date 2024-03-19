5114c1953f81eb20e5c605fa77662bdaab32ec67
Moses-Morris <mosesmorrisdev@gmail.com>
2024-03-19 10:35:55 +0300

    Creating a Signaller and receiver for our profile image.

---
+++ FeaturedApp/users/signals.py
@@ -0,0 +1,16 @@
+from django.db.models.signals import post_save #fired after user is saved
+from django.contrib.auth.models import User #sender of the signal
+from django.dispatch import receiver #receiver of the signal
+from .models import Profile
+
+
+@receiver(post_save, sender=user)
+def create_profile(sender, instance, created, **kwargs):
+    if created:
+        Profile.objects.create(user=instance)
+
+
+#save profile when user is saved
+@receiver(post_save, sender=user)
+def create_profile(sender, instance,  **kwargs):
+    instance.profile.save()
